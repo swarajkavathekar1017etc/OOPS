a641e9f0c02e9d86ec9fb54433c85451602d055a
swarajkavathekarb25et1017-afk <swarajkavathekar2025.etc@mmcoe.edu.in>
2026-09-15 03:13:16 +0000

    TP

---
+++ 1.CPP
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+int rows;
+cout << "Enter number of rows: ";
+cin >> rows;
+for (int i = 0; i < rows; i++) {
+int num = 1;
+for (int j = 0; j < rows - i; j++)
+cout << " "; // spacing
+for (int k = 0; k <= i; k++) {
+cout << num << " ";
+num = num * (i - k) / (k + 1);
+}
+cout << endl;
+}
+return 0;
+}
